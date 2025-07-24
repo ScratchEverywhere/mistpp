b7e3544bca792f34631427f5ebdaf15f1b35a8d9
Grady Link <github@grady.link>
2025-07-23 20:36:03 -0700

    feat: add more boilerplate

--- include/mist/mist.hpp
+++ include/mist/mist.hpp
@@ -1,6 +1,19 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace mist {
 void init();
 void quit();
+
+class Connection {
+private:
+  std::vector<std::string> cloudHosts;
+  std::string projectID;
+  std::string username;
+  std::string userAgent;
+
+  Connection(std::vector<std::string> cloudHosts, std::string projectID, std::string username, std::string userAgent) : cloudHosts(cloudHosts), projectID(projectID), username(username), userAgent(userAgent) {}
+};
 } // namespace mist
