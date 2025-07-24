a5142f87e79a7e72fcd53ac913aa21007f4550d3
Grady Link <github@grady.link>
2025-07-23 18:45:16 -0700

    feat: init actual code

---
+++ include/mist/mist.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace mist {
+void init();
+void quit();
+} // namespace mist
---
+++ src/mist.cpp
@@ -0,0 +1,6 @@
+#include "mist/mist.hpp"
+
+namespace mist {
+void init() {}
+void quit() {}
+} // namespace mist
